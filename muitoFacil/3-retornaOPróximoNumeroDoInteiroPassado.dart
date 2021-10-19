a38d4d0424316a8414e64ff24bf370fc294b7c8a
acorreac <aline.cristianecorrea@hotmail.com>
2021-10-19 12:05:27 -0300

    Retorna o proximo numero inteiro

---
+++ muitoFacil/3-retornaOPróximoNumeroDoInteiroPassado.dart
@@ -0,0 +1,17 @@
+// Crie uma função que recebe um número como argumento, incrementa o número em +1 e retorna o resultado.
+//Exemplos
+//addition(0) ➞ 1
+//addition(9) ➞ 10
+//addition(-3) ➞ -2
+
+//Função de inicialização do programação que não recebe parâmetro (void) e não contém retorno.
+void main() {
+  //Declaração a função *addition* do tipo int que recebe o parâmetro *num*.
+  int addition(num) {
+//Retorna a soma do valor do parâmetro *num* com o valor 1.
+    return num + 1;
+  }
+
+//Recebe o valor do parâmetro da função *adition* e exibe na saída do console o retorno da mesma função.
+  print(addition(5));
+}
