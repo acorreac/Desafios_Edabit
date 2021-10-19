856153768721c9d1011a2e0392037d1e9d16bbff
acorreac <aline.cristianecorrea@hotmail.com>
2021-10-19 12:10:32 -0300

    Calculadora de potencia

---
+++ muitoFacil/8-powerCalculator.dart
@@ -0,0 +1,13 @@
+//Criar uma função que recebe voltagee currente retorna o calculado poder.
+
+//Função de inicialização do programação.
+void main() {
+//Função *circuitPower* do tipo *int*, parâmetros *voltage, current*.
+  int circuitPower(voltage, current) {
+//Retorna a multiplicação dos valores recebi nos parâmetros da função.
+    return voltage * current;
+  }
+
+//Recebe os valores dos parâmetros da função e exibe na saída do console o retorno da mesma função.
+  print(circuitPower(220, 10));
+}
