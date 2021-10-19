db63f4e307dd42ef78d29c3d17a76c7d33eff044
acorreac <aline.cristianecorrea@hotmail.com>
2021-10-19 12:06:39 -0300

    Converte idade em dias

---
+++ muitoFacil/5-converterIdadeEmDias.dart
@@ -0,0 +1,13 @@
+//Crie uma função que obtenha a idade em anos e retorne a idade em dias.
+
+//Função de inicialização do programação.
+void main() {
+//Função *calcAge* do tipo *int*, parâmetros *age*.
+  int calcAge(age) {
+//Retorna a multiplicação de *age* por 365(dias).
+    return age * 365;
+  }
+
+//Recebe o valor do parâmetros da função e exibe na saída do console o retorno da mesma função.
+  print(calcAge(25));
+}
