53a0e138744dc1ca3e92ab334951909e14d5e271
acorreac <aline.cristianecorrea@hotmail.com>
2021-10-19 12:25:06 -0300

    Retorna a string 'something' unida de um espaço e um argumento fornecido

---
+++ muitoFacil/13-devolvaAlgoParaMim.dart
@@ -0,0 +1,19 @@
+/*Escreva uma função que retorne a string "something"unida com um espaço " "e o argumento fornecido a.
+
+Exemplos
+giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+giveMeSomething("something") ➞ "something something"
+*/
+
+//Função de inicialização do programação.
+void main() {
+//Declaração a função *something* do tipo *String* que recebe o parâmetro *a*.
+  String something(a) {
+//Retorna a mesagem em aspas e o valor do parâmetro *a*.
+    return "Something $a";
+  }
+
+//Recebe o valor dos parâmetros da função e exibe na saída do console o retorno da mesma função.
+  print(something('Luan'));
+}
