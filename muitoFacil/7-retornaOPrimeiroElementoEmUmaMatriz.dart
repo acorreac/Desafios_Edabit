91383cd29a04f4e33ddf0a1de2f13671a85263f2
acorreac <aline.cristianecorrea@hotmail.com>
2021-10-19 12:09:57 -0300

    Retorna o primeiro elemento em uma matriz

---
+++ muitoFacil/7-retornaOPrimeiroElementoEmUmaMatriz.dart
@@ -0,0 +1,13 @@
+//Crie uma função que pegue uma matriz contendo apenas números e retorne o primeiro elemento.
+
+//Função de inicialização do programação.
+void main() {
+//Função *getFirstValue* do tipo *int*, parâmetro *arr*.
+  int getFirstValue(arr) {
+//Retorna o elemento contido no índice 0 da arr.
+    return arr[0];
+  }
+
+//Recebe o valores do parâmetro *arr* da função e exibe na saída do console o retorno da mesma função.
+  print(getFirstValue([9, 4, 6, 7]));
+}
